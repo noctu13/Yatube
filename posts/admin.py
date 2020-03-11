578c7af9e5bbaf6be89b4b02946e0a4a39e058ee
Sapraliev Mikhail <13bit57@gmail.com>
2020-03-11 12:23:26 +0300

    Release 0.0.3

--- posts/admin.py
+++ posts/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Group
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ("text", "pub_date", "author")
+    list_display = ("pk", "text", "pub_date", "author")
     search_fields = ("text",)
     list_filter = ("pub_date",)
-    empty_value_display = "-пусто-"
+    empty_value_display = '-пусто-'
 
 admin.site.register(Post, PostAdmin)
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ("pk", "title", "slug", "description")
+    search_fields = ("title",)
+    empty_value_display = '-пусто-'
+    prepopulated_fields = {"slug": ("title",)}
+
+admin.site.register(Group, GroupAdmin)
--- posts/urls.py
+++ posts/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("group/<str:slug>", views.group_posts, name="group"),
+    path("groups", views.group_list, name="group_list")
 ]
