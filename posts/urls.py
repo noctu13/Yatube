97c11d5394f963d1731771c74cd2941d3937dc39
Sapraliev Mikhail <13bit57@gmail.com>
2020-03-11 20:33:25 +0300

    Release 0.0.4

--- posts/urls.py
+++ posts/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("group/<str:slug>", views.group_posts, name="group"),
-    path("groups", views.group_list, name="group_list")
+    path("groups", views.group_all, name="group_all"),
+    path("new", views.new_post, name="new_post")
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Group
+from .forms import PostForm
 
 def index(request):
     latest = Post.objects.order_by("-pub_date")[:11]
@@ -10,6 +11,16 @@ def group_posts(request, slug):
     posts = Post.objects.filter(group=group).order_by("-pub_date")[:12]
     return render(request, "group.html", {"group": group, "posts": posts})
 
-def group_list(request):
+def group_all(request):
     groups = Group.objects.all()
-    return render(request, "group_list.html", {"groups": groups})
+    return render(request, "group_all.html", {"groups": groups})
+
+def new_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+        return render(request, "new_post.html", {"form": form})
+    form = PostForm()
+    return render(request, "new_post.html", {"form": form})
