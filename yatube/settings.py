8e3bb8f8c98c4567024ffc851b9805fd51932a31
Sapraliev Mikhail <13bit57@gmail.com>
2020-03-20 11:54:43 +0300

    Release 0.0.9

--- yatube/settings.py
+++ yatube/settings.py
@@ -42,7 +42,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "django.contrib.sites",
+    "django.contrib.flatpages",
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
