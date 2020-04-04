6f35454eed66120aa0fbab3f89169e08d2b5ee40
Sapraliev Mikhail <13bit57@gmail.com>
2020-04-04 10:24:18 +0300

    Release 0.1.5r test

--- posts/views.py
+++ posts/views.py
@@ -4,27 +4,26 @@ from django.contrib.auth.decorators import login_required
 from .models import Post, Group, User
 from .forms import PostForm
 
+class PageBack:
+    def __init__(self, request, _list):
+        self.paginator = Paginator(_list, 10)
+        self.page = self.paginator.get_page(request.GET.get('page'))
+
 def index(request):
     post_list = Post.objects.order_by("-pub_date").all()
-    paginator = Paginator(post_list, 10)
-    page_number = request.GET.get('page')
-    page = paginator.get_page(page_number)
-    return render(request, 'index.html', {'page': page, 'paginator': paginator})
+    page_back = PageBack(request, post_list)
+    return render(request, 'index.html', {'page': page_back.page, 'paginator': page_back.paginator})
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     post_list = Post.objects.filter(group=group).order_by("-pub_date")
-    paginator = Paginator(post_list, 10)
-    page_number = request.GET.get('page')
-    page = paginator.get_page(page_number)
-    return render(request, "group.html", {"group": group, 'page': page, 'paginator': paginator})
+    page_back = PageBack(request, post_list)
+    return render(request, "group.html", {"group": group, 'page': page_back.page, 'paginator': page_back.paginator})
 
 def group_all(request):
     group_list = Group.objects.all()
-    paginator = Paginator(group_list, 10)
-    page_number = request.GET.get('page')
-    page = paginator.get_page(page_number)
-    return render(request, "group_all.html", {'page': page, 'paginator': paginator})
+    page_back = PageBack(request, group_list)
+    return render(request, "group_all.html", {'page': page_back.page, 'paginator': page_back.paginator})
 
 @login_required
 def new_post(request):
@@ -40,44 +39,30 @@ def new_post(request):
     return render(request, "new_post.html", {"form": form})
 
 def profile_view(request, username):
-    profile = get_object_or_404(User, username=username)
-    post_list = Post.objects.filter(author=profile).order_by("-pub_date")
-    paginator = Paginator(post_list, 10)
-    page_number = request.GET.get('page')
-    page = paginator.get_page(page_number)
-    return render(request, "profile.html", {"profile": profile, 'page': page, 'paginator': paginator})
+    author = get_object_or_404(User, username=username)
+    post_list = Post.objects.filter(author=author).order_by("-pub_date")
+    page_back = PageBack(request, post_list)
+    return render(request, "profile.html", {"author": author, 'page': page_back.page, 'paginator': page_back.paginator})
 
 def post_view(request, username, post_id):
-    profile = get_object_or_404(User, username=username)
-    queryset = Post.objects.filter(author=profile).order_by("pub_date")
-    post_dict = dict(enumerate(queryset))
-    try:
-        post = post_dict[post_id]
-    except KeyError:
+    author = get_object_or_404(User, username=username)
+    post = get_object_or_404(Post, pk=post_id)
+    if author != post.author:
         return render(request, "404.html")
-    return render(request, "post.html", {"profile": profile, "post": post})
+    return render(request, "post.html", {"author": author, "post": post})
 
 @login_required
 def post_edit(request, username, post_id):
-    if request.user == get_object_or_404(User, username=username):
-        queryset = Post.objects.filter(author=request.user).order_by("pub_date")
-        post_dict = dict(enumerate(queryset))
-        try:
-            post = post_dict[post_id]
-        except KeyError:
-            return render(request, "404.html")
-        #тут может случится потенциальная серверная ошибка?
-    else:
-        #return render(request, "403.html") было бы логичнее
+    author = get_object_or_404(User, username=username)
+    if request.user != author:
         return redirect('post', username=username, post_id=post_id)
-    #здесь вроде можно использовать вьюху new_post, если передавать post
+    post = get_object_or_404(Post, pk=post_id)
     if request.method == 'POST':
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect('index')
+            form.save()
+            return redirect('post', username=username, post_id=post_id)
     else:
         form = PostForm(instance=post)
-    return render(request, "edit_post.html", {"form": form})
+    return render(request, "edit_post.html", {"form": form, "post": post})
+    
--- yatube/tests.py
+++ yatube/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase, Client
 from posts.models import User, Post
 
-class TestCoverage(TestCase):
+class PostViewsTestCase(TestCase):
     def setUp(self):
         self.client = Client()
         self.user = User.objects.create_user(
@@ -12,40 +12,40 @@ class TestCoverage(TestCase):
             last_name='macloud'
         )
 
-    def test_Profile(self):
+    def test_profile(self):
         response = self.client.get('/duncan/')
         self.assertEqual(response.status_code, 200)
 
-    def test_newPostCreation(self):
+    def test_post_creation(self):
         self.client.force_login(self.user)
         response = self.client.get('/new/')
         self.assertEqual(response.status_code, 200)
 
-    def test_newPostCreationForbidden(self):
+    def test_post_creation_forbidden(self):
         response = self.client.get('/new/', follow=True)
         self.assertRedirects(response, '/auth/login/?next=%2Fnew%2F')
         
-    def test_newPost(self):
+    def test_post(self):
         text = 'some incoherent text'
-        Post.objects.create(text=text, author=self.user)
+        post = Post.objects.create(text=text, author=self.user)
         response = self.client.get('')
         self.assertContains(response, text)
         response = self.client.get('/duncan/')
         self.assertContains(response, text)
-        response = self.client.get('/duncan/0/')
+        response = self.client.get('/duncan/%s/' % post.pk)
         self.assertContains(response, text)
 
-    def test_postEdit(self):
+    def test_post_edit(self):
         text = 'some incoherent text'
-        Post.objects.create(text=text, author=self.user)
+        post = Post.objects.create(text=text, author=self.user)
         text = 'soMe inCoherent ediTed teXt'
         self.client.force_login(self.user)
-        self.client.post('/duncan/0/edit/', {'text': text, 'group': ''})
+        self.client.post('/duncan/%s/edit/' % post.pk, {'text': text, 'group': ''})
         response = self.client.get('')
         self.assertContains(response, text)
         response = self.client.get('/duncan/')
         self.assertContains(response, text)
-        response = self.client.get('/duncan/0/')
+        response = self.client.get('/duncan/%s/' % post.pk)
         self.assertContains(response, text)
 
     def cleanUp(self):
