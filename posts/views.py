4a6ad4d24c95b945e8d9ac1b54f20139d757802d
Sapraliev Mikhail <13bit57@gmail.com>
2020-04-11 14:54:30 +0300

    Release 0.3.1

--- posts/views.py
+++ posts/views.py
@@ -94,7 +94,8 @@ def follow_index(request):
 @login_required
 def profile_follow(request, username):
     author = get_object_or_404(User, username=username)
-    Follow.objects.create(user=request.user, author=author)
+    if author != request.user:
+        Follow.objects.create(user=request.user, author=author)
     return redirect('profile', username=username)
 
 @login_required
